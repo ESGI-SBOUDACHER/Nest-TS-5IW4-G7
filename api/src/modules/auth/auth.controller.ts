ae819620f9cb073b97d4f061c7a25b8a9914cf05
Romain Pierucci <pierucci.romain@gmail.com>
2023-04-01 19:20:36 +0200

    feat: all routes protected by default, need @public decorator to determine a public route

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,36 +1,23 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Request,
-  UseGuards,
-  Version,
-} from '@nestjs/common';
+import { Public } from '@api/common/decorators/public';
+import { Body, Controller, Post, Version } from '@nestjs/common';
 import { LoginDto, RegisterDto } from './auth.dto';
-import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Version('1')
   @Post('login')
   login(@Body() body: LoginDto) {
     return this.authService.login(body.email, body.password);
   }
 
+  @Public()
   @Version('1')
   @Post('register')
   register(@Body() body: RegisterDto) {
     return this.authService.register(body.email, body.password);
-  }
-
-  @Version('1')
-  @UseGuards(AuthGuard)
-  @Get('profile')
-  getProfile(@Request() req: any) {
-    return req.user;
   }
 }
--- api/src/modules/auth/auth.module.ts
+++ api/src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 @Module({
   imports: [
@@ -9,11 +10,18 @@ import { AuthService } from './auth.service';
     JwtModule.register({
       global: true,
       secret: process.env.JWT_SECRET,
+      // TODO : Add some expiration time at the end of the project
       // signOptions: { expiresIn: '60s' },
     }),
   ],
 
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
