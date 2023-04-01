3a0eaade1cafa71912e7124c7b936544d54d6844
Romain Pierucci <pierucci.romain@gmail.com>
2023-04-01 11:40:31 +0200

    feat(auth): JWT implementation + NestJS conf

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,18 +1,24 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   // TODO : Use bcrypt here
   async login(email: string, password: string): Promise<any> {
     const user = await this.userService.getUser({ where: { email } });
     if (user && user.password === password) {
-      const { password, ...result } = user;
-      // TODO : return JWT token instead
-      return result;
+      const payload = { email: user.email, id: user.id, role: user.role };
+      return {
+        token: await this.jwtService.signAsync(payload),
+      };
     }
+
     return UnauthorizedException;
   }
 }
