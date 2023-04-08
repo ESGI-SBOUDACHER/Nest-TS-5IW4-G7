b9b41dd0727c89656dde3918c244e4199e603a4a
Coraline <cesedji@myges.fr>
2023-04-08 14:08:09 +0200

    add : update article

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,11 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { ZodValidationPipe } from 'nestjs-zod';
+import { ArticlesModule } from './modules/articles/articles.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { CategoriesModule } from './modules/categories/categories.module';
-import { UsersModule } from './modules/users/users.module';
 import { CommentsModule } from './modules/comments/comments.module';
-import { ArticlesModule } from './modules/articles/articles.module';
+import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     AuthModule,
